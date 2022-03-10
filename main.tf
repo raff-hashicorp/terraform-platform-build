d101978096c81e89f30a72d3a118f5f656ef453d
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-03-10 11:49:47 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+//--------------------------------------------------------------------
+// Modules
+module "workspace" {
+  source  = "app.terraform.io/bns-terraform-workshop-rs/workspace/tfc"
+  version = "0.1.0-alpha"
+
+  appid = "APP-ABCD"
+  hostname = "app.terraform.io"
+  prefix = "proj-sb"
+  tfcb_org = "bns-terraform-workshop-rs"
+}
